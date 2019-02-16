8ab74526773bd74e5a9cf17d94c0c4e6dcab7463
Taylor Pechacek <taylortpechacek@gmail.com>
2019-02-17 07:49:49 +1100

    set up first reducer and seeded an array of Library data. Now wiring that up to my connect function so it is aware of redux store and it's state

---
+++ src/components/common/index.js
@@ -0,0 +1,6 @@
+export * from './Button'; // This imports and exports in one step
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
+export * from './Input';
+export * from './Spinner';
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 // This is the window into all other reducers.
 
 import { combineReducers } from 'redux';
+import LibraryReducer from './LibraryReducer';
 
 export default combineReducers({
-  libraries: () => []
+  libraries: LibraryReducer
 });
