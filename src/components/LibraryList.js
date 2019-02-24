ae15d35657dbc3313519e1251262fa7e0ffdece3
Taylor Pechacek <taylortpechacek@gmail.com>
2019-02-24 11:03:26 +1100

    learning more about lists and how to handle a large number of items effectively

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import { FlatList } from 'react-native';
+import { connect } from 'react-redux';
 
 class LibraryList extends Component {
+  renderItem(library) {}
+
   render() {
-    return;
+    return (
+      <FlatList
+        data={this.props.libraries}
+        renderItem={this.renderItem}
+        keyExtractor={library => library.id}
+      />
+    );
   }
 }
 
-export default LibraryList;
+const mapStateToProps = state => {
+  return { libraries: state.libraries };
+};
+
+export default connect(mapStateToProps)(LibraryList);
